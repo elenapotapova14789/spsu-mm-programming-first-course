337de799b1b225d6b2676b78bc5e432cb31cd3dc
usik-vladislav <usik.vladislav.s@gmail.com>
2017-09-20 00:51:23 +0300

    Added Task2

---
+++ Autumn/Task2/Task2/algoritm_Evklida.c
@@ -0,0 +1,25 @@
+#include "stdafx.h"
+
+int NODNumbers(int x, int y)
+{
+	int nominator = 0;
+	int denominator = 0;
+	if (x > y)
+	{
+		nominator = x;
+		denominator = y;
+	}
+	else
+	{
+		nominator = y;
+		denominator = x;
+	}
+	if (nominator % denominator != 0)
+	{ 
+		return NODNumbers(denominator, nominator % denominator);
+	}
+	else
+	{
+		return denominator;
+	}
+}
