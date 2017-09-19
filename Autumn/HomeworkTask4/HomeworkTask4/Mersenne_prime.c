6780ad862d5e36966560ca9525f6c7dc35d9837b
sultim222 <sultim.tsyrendashiev@gmail.com>
2017-09-19 21:50:00 +0300

    - added first version of fourth task

---
+++ Autumn/HomeworkTask4/HomeworkTask4/Mersenne_prime.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int check_prime(int);
+
+int main()
+{
+	int a = 1;
+
+	printf("Mersenne primes:\n");
+
+	for (int n = 1; n <= 31; n++)
+	{
+		a *= 2;
+		if (check_prime(a - 1))
+			printf("%d\n", a - 1);
+	}
+
+	return 0;
+}
+
+int check_prime(int a)
+{
+	for (int i = 2; i < a; i++)
+		if (a % i == 0)
+			return 0;
+	return 1;
+}
